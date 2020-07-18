a605f0b78d074c40a242fe3de7f445571238554a
SergioF <fs220381@gmail.com>
2020-07-18 21:10:40 +0300

    Добавьте файлы проекта.

---
+++ GameSetMatch/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSetMatch
+{
+    class Game
+    {
+        private Player firstPlayer;
+        private Player secondPlayer;
+
+        private int firstPlayerScore = 0;
+        private int secondPlayerScore = 0;
+
+        public bool Tie { get; set; }
+
+        public Game(string firstPlayerName, string secondPlayerName)
+        {
+            firstPlayer = new Player(firstPlayerName);
+            secondPlayer = new Player(secondPlayerName);
+        }
+
+        public void AddPoint(string playerName)
+        {
+            firstPlayerScore = playerName == firstPlayer.PlayerName ? firstPlayerScore + 1 : firstPlayerScore;
+            secondPlayerScore = playerName == secondPlayer.PlayerName ? secondPlayerScore + 1 : secondPlayerScore;
+        }
+
+        public bool IsEndGame()
+        {
+            if((firstPlayerScore >= 4 || secondPlayerScore >= 4) &&(Math.Abs(firstPlayerScore - secondPlayerScore) >= 2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string NameWinners()
+        {
+
+        }
+    }
+}
---
+++ GameSetMatch/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSetMatch
+{
+    class Player
+    {
+        public string PlayerName { get; set; }
+
+        public Player(string name)
+        {
+            PlayerName = name;
+        }
+    }
+}
